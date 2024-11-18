7e936f3384e1dc8ddf62f64819cda4b4b28ba1ad
Andrey Pertcev <school299.frn@gmail.com>
2024-11-18 11:32:27 +0300

    Update lab_2.py

--- lab_2.py
+++ lab_2.py
@@ -1,4 +1,5 @@
 import csv
+import xml.dom.minidom
 from random import randint
 
 def task1():
@@ -15,7 +16,7 @@ def task1():
 
 def task2():
     while True:
-        text = '---Людмила ПетрановскаяНе найдено---'
+        text = '---Не найдено---'
         print('Введите автора или напишите "далее"')
         avtor = input()
         if avtor == 'далее':
@@ -40,10 +41,34 @@ def task3():
         result = open('result.txt', 'w')
         for i in range(20):    
             number = randint(1, 9400)
-            result.write(f"{i + 1} {reader[number]['Автор']} - {reader[number]['Название']} - {int(str(reader[number]['Дата поступления'])[6:10])} \n") 
-
+            result.write(f"{i + 1} {reader[number]['Автор']} - {reader[number]['Название']} - {int(str(reader[number]['Дата поступления'])[6:10])} \n")
+            
+def task4():
+    file = open('currency.xml', 'r')
+    data = file.read()
+    dom = xml.dom.minidom.parseString(data)
+    dom.normalize()   
+    elements = dom.getElementsByTagName('Valute')
+    valute_disk = {}
+    for node in elements:
+        for child in node.childNodes:
+            if child.nodeType == 1:
+                if child.tagName == 'Name':
+                    if child.firstChild.nodeType == 3:
+                        Name = child.firstChild.data
+                if child.tagName == 'CharCode':
+                    if child.firstChild.nodeType == 3:
+                        Code = child.firstChild.data
+        valute_disk[Name] = Code
+    for key in valute_disk.keys():
+        print(key,'-', valute_disk[key])
+    file.close()
 print('задание 1')
 task1()
 print('задание 2')
 task2()
 task3()
+print('задание 3')
+print('файл создан')
+print('задание 4')
+task4()
